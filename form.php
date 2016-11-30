b2e16179c51e9dd9d8d719efa0d7d79cd06bb05d
ISChetverikov <ischetverikov@gmail.com>
2016-11-30 14:52:41 +1000

    Реализована проверка целостности сообщения HMAC

--- form.php
+++ form.php
@@ -15,21 +15,24 @@ if (!Validator::validateOrderId($_GET['OrderId'])) {
 
 session_start();
 
-$_SESSION['secret'] = md5(rand(0,1000).rand());//
-$salt = rand(0,1000);
+$_SESSION['secret'] = md5(rand(0, 1000).rand());//
+$salt = rand(0, 1000);
 $token = hash('sha256', $salt.$_SESSION['secret']);
 
 echo <<<HTML
-    <script type="text/javascript" src="hmac-sha256.js"></script>
-    <script type="text/javascript">
-    function send(){  
-        var HMAC = CryptoJS.HmacSHA256("Message", "Secret Passphrase");
-        alert(HMAC);
-    }
-   
- </script>
+<script type="text/javascript" src="hmac-sha256.js"></script>
+<script type="text/javascript">
+function send(){
+    var amount = document.payment.Amount.value; 
+    var orderId = document.payment.OrderId.value;
+    var token = document.payment.token.value;
+    var data = amount.toString()+orderId.toString()+token;
+    var HMAC = CryptoJS.HmacSHA256(data , "Key");
+    document.payment.HMAC.value = HMAC;
+}
+</script>
 
-<form action="result.php" method="post" onsubmit="return send()">
+<form name="payment" action="result.php" method="post" onsubmit="return send()">
     <p>Amount: <input type="text" name="Amount" required/></p>
     <p><input type="hidden" name="HMAC" /></p>
     <p><input type="hidden" name="OrderId" value=$_GET[OrderId] />
