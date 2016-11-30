5d62ec196d9fa0f0129b99f5cb8dfb28d469cbcd
ISChetverikov <ischetverikov@gmail.com>
2016-11-30 17:01:51 +1000

    Переименовал название ключа CSRF

--- result.php
+++ result.php
@@ -3,28 +3,40 @@ require_once "validator.php";
 
 session_start();
 
-if (!isset($_SESSION['secret']) || !isset($_POST['token'])) {
-    echo "<p>Попытка отправки данных в обход официальной формы1!</p>";
+if (!isset($_SESSION['keyCsrfToken']) || !isset($_POST['token']) || !isset($_SESSION['keyHmac'])) {
+    echo "<p>Попытка отправки данных в обход официальной формы!</p>";
     exit();
 }
 
 $tokenArr = explode(":", $_POST['token']);
 if (count($tokenArr) != 2) {
-    echo "<p>Попытка отправки данных в обход официальной формы2!</p>";
+    echo "<p>Попытка отправки данных в обход официальной формы!</p>";
     exit();
 }
 $salt = $tokenArr[0];
 $token = $tokenArr[1];
 
-$realToken = hash('sha256', $salt.$_SESSION['secret']);
+$realToken = hash('sha256', $salt.$_SESSION['keyCsrfToken']);
 
 if ($realToken != $token) {
-    echo "<p>Попытка отправки данных в обход официальной формы3!</p>";
+    echo "<p>Попытка отправки данных в обход официальной формы!</p>";
+    exit();
+}
+
+if (!isset($_POST['HMAC'])) {
+    echo "<p>Данные прибыли без подтверждения подлинности!</p>";
     exit();
 }
 
 if (!isset($_POST['OrderId']) || !isset($_POST['Amount'])) {
     echo "<p>Необходимые данные с формы не прибыли!</p>";
+    exit();
+}
+
+$dataHMAC = $_POST['Amount'].$_POST['OrderId'].$_POST['token'];
+$realHMAC = hash_hmac('sha256', $dataHMAC, $_SESSION['keyHmac']);
+if ($realHMAC != $_POST['HMAC']) {
+    echo "<p>Неверный ключ подтверждения!</p>";
     exit();
 }
 
@@ -35,9 +47,8 @@ if (!Validator::validateOrderId($_POST['OrderId'])
     exit();
 }
 
-unset($_SESSION['secret']);
-echo $token;
-echo "<br/>";
-echo $realToken;
+unset($_SESSION['keyCsrfToken']);
+unset($_SESSION['keyHmac']);
+
 echo "<p>Данные корректны!</p>";
 ?>
